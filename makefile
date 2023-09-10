6aac7c0ae145294f88cdc8e916eeb63deae42ceb
Daniel Williams <equipoise@gmail.com>
2023-09-10 18:42:57 -0400

    [xp][i2c] initial commit of circuit-python utils, for interfacing with the i2c adc (ADS7830)

--- makefile
+++ makefile
@@ -1,32 +1,43 @@
 
-
-
-
 # ===================================================
-
 default:build
 
 clean:
 	rm -rf build/*
 
-.PHONY:configure
-configure:
+.PHONY:config
+config: build/CMakeCache.txt
+
+build/CMakeCache.txt:
 	cd build && cmake ..
 
+build/irmon.uf2: build
+
 .PHONY:build
-build:
+build: build/CMakeCache.txt $(SRCS)
 	cd build && make
 
 .PHONY:mon
-mon:
+mon: 
 	stty -F /dev/ttyACM0 115200
 	screen /dev/ttyACM0 
 
-.PHONY:reboot
-reboot:
-	picotool reboot --usb --bus 3 --address 19 --force
+reboot:reset
+
+.PHONY:reset
+reset:
+	$(eval ADDR:=$(shell lsusb | grep Pico | cut -c16-18))
+	@echo ":> Rebooting device at Address:${ADDR}"
+	picotool reboot --usb --bus 3 --address ${ADDR} --force
+	sleep 3
+
+.PHONY:run
+run: reboot upload
 
 .PHONY:upload
-upload: 
+upload: build/irmon.uf2
 	cp build/irmon.uf2 /media/teyrana/RPI-RP2/
+	sleep 2
 
+cpy:
+	cp tools/i2c-scan.py /media/teyrana/CIRCUITPY/main.py
--- src/irmon/main.c
+++ src/irmon/main.c
@@ -9,73 +9,68 @@
 
 #include "receive.h"
 
-
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
+void init_activity_led(){
+    // use the led as an activity indicator
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+}
 
 int main(){
 
     //set_sys_clock_48();
     stdio_init_all();
 
-    while (true) {
-
-        gpio_put(LED_PIN, 1);
-        sleep_ms(1000);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(1000);
-
-        // Print Out Debug
-        printf("Hello, world!\n");
-
-    }
-#endif
-}
+    init_activity_led();
 
 
-int main() {
-    stdio_init_all();
+    const uint rx_gpio = 6;   // choose which GPIO pin is connected to the IR detector
+    PIO rx_pio = pio0;         // choose which PIO block to use (RP2040 has two: pio0 and pio1)
 
-    // use the led as an activity indicator
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    PIO pio = pio0;                                 // choose which PIO block to use (RP2040 has two: pio0 and pio1)
-    uint tx_gpio = 14;                              // choose which GPIO pin is connected to the IR LED
-    uint rx_gpio = 15;                              // choose which GPIO pin is connected to the IR detector
+    // const uint tx_gpio = 14;      // choose which GPIO pin is connected to the IR LED
+    // PIO  tx_pio = pio0;       // (sufficient state-machines are available)
 
     // configure and enable the state machines
-    int tx_sm = nec_tx_init(pio, tx_gpio);         // uses two state machines, 16 instructions and one IRQ
-    int rx_sm = nec_rx_init(pio, rx_gpio);         // uses one state machine and 9 instructions
+    // int tx_sm = nec_tx_init( tx_pio, tx_gpio);         // uses two state machines, 16 instructions and one IRQ
+    const int rx_sm = nec_rx_init( rx_pio, rx_gpio);         // uses one state machine and 9 instructions
 
-    if (tx_sm == -1 || rx_sm == -1) {
-        printf("could not configure PIO\n");
-        return -1;
-    }
 
     // transmit and receive frames
-    uint8_t tx_address = 0x00, tx_data = 0x00, rx_address, rx_data;
+    uint8_t rx_address = 0x00;
+    uint8_t rx_command = 0x00;
+
+    bool led_state = true;
+
+    //, rx_address, rx_data; // Not Implemented
     while (true) {
-        // create a 32-bit frame and add it to the transmit FIFO
-        uint32_t tx_frame = nec_encode_frame(tx_address, tx_data);
-        pio_sm_put(pio, tx_sm, tx_frame);
-        printf("\nsent: %02x, %02x", tx_address, tx_data);
 
-        // allow time for the frame to be transmitted (optional)
-        sleep_ms(100);
-
-        // display any frames in the receive FIFO
-        while (!pio_sm_is_rx_fifo_empty(pio, rx_sm)) {
-            uint32_t rx_frame = pio_sm_get(pio, rx_sm);
-
-            if (nec_decode_frame(rx_frame, &rx_address, &rx_data)) {
-                printf("\treceived: %02x, %02x", rx_address, rx_data);
-            } else {
-                printf("\treceived: %08x", rx_frame);
-            }
+        // if (tx_sm == -1 || rx_sm == -1) {
+        if( rx_sm == -1 ){
+            printf("could not configure PIO state-machine!!\n");
+            sleep_ms(5000);
+            continue;
         }
 
-        sleep_ms(900);
-        tx_data += 1;
+        // display any frames in the receive FIFO
+        while (!pio_sm_is_rx_fifo_empty(rx_pio, rx_sm)) {
+            uint32_t rx_frame = pio_sm_get(rx_pio, rx_sm);
+
+            if (nec_decode_frame(rx_frame, &rx_address, &rx_command)) {
+                printf("\t:rx:ok: %02x // %02xh (%d)\n", rx_address, rx_command, rx_command );
+            } else {
+                printf("\t:rx:bad: %08x\n", rx_frame);
+            }
+
+            gpio_put(LED_PIN, 1);
+            sleep_ms(120);
+            gpio_put(LED_PIN, 0);
+        }
+
+        // sleep_ms(200);
+        // gpio_put(LED_PIN, (int)led_state);
+        // led_state = ! led_state;
+
+        // tx_data += 1;
     }
 }
