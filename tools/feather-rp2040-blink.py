2659ffc74a6ba08dfdc2e0b6ccea9a9b3838ee62
Daniel Williams <equipoise@gmail.com>
2024-05-05 12:29:01 -0400

    [rfr][mv] rename feather blink script

--- tools/feather-rp2040-blink.py
+++ tools/feather-rp2040-blink.py
@@ -22,11 +22,13 @@ import neopixel
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 while True:
-    pixels.fill((64, 0, 0))
-    time.sleep(0.1)
-    pixels.fill((0, 64, 0))
-    time.sleep(0.1)
-    pixels.fill((0, 0, 64))
-    time.sleep(0.1)
+    pixels.fill((128, 0, 0))
+    time.sleep(1)
+    pixels.fill((0, 128, 0))
+    time.sleep(1)
+    pixels.fill((0, 0, 128))
+    time.sleep(1)
+
     pixels.fill((0, 0, 0))
-    time.sleep(1.4)
+
+    time.sleep(7)
